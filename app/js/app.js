cde6abd00c76de00fdee4087e002ccaf534831f3
Farmin Farzin <farmin.f@gmail.com>
2017-02-21 15:15:42 +0100

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -5,34 +5,31 @@
  * on which the app depends, i.e., of widgets used by the app.
  */
 
-var app = angular.module('app', ['ngRoute' , 'dashNavigation']);
+var application = angular.module('application', ['ngRoute' , 'dashNavigation' , 'restangular']);
 
 // declare the routes
-app.config([ '$routeProvider', function($routeProvider) {
+application.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl : 'views/home.html'
 	});
 	$routeProvider.otherwise({
 		redirectTo : '/'
-	});
-} ]);
+	});	
+}]);
 
 // the app controller
-// app.controller('skeletonCtrl', // controller name
-// 	['$scope',
-// 	'Restangular',
-// 	'$attrs', 
-// 	function($scope, Restangular, $attrs) {
+application.controller('skeletonCtrl', //controller name
+	['$scope',
+	'Restangular',
+	'$attrs', 
+	function($scope, Restangular, $attrs) {
+		var ctrl = this;	
+}]);
 
-							
-// } ]);
-
-
-
-
-// app.config(function(RestangularProvider) {
-// 	RestangularProvider
-// 			.setBaseUrl('http://localhost:8080/api');
-// });
+//Configure restangular base url
+application.config(function(RestangularProvider) {
+	RestangularProvider
+			.setBaseUrl('http://localhost:8080/api');
+});
 
 
